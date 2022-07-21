69e03d8720dadb6c3dc18de4aaf5a8e03ee0b9c6
AaronsonRossita <aaronsonrossita@gmail.com>
2022-07-21 21:22:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        int x = sum(3,7);
+//        sum(7,8);
+//        System.out.println(x);
+//        int sub = sub(5,6);
+//        System.out.println(sub);
+
+        System.out.println(sub(5,6));
+
+        printSomething();
+
+    }
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+
+    public static void printSomething(){
+        System.out.println("Something");
+    }
+
+
+}
+
